a09c7f4bb202d0325acc238808ee41a919c57be5
ewen_ <ewenhenderson@gmail.com>
2018-06-06 21:34:19 +0100

    add docs

--- R/print.R
+++ R/print.R
@@ -20,8 +20,8 @@ print.discogs_database <- function(x, ...) {
 print.discogs_collection <- function(x, ...) {
 
   cat("<Discogs ", x$path, ">\n", sep = "")
-  cat("Preview: 1 of", length(foo$content), "records.", "\n", sep=" ")
-  str(x$content[[1]])
+  cat("Preview: 1 of", length(x$content), "records.", "\n", sep=" ")
+  str(head(x$content, 1))
   invisible(x)
 
 }
